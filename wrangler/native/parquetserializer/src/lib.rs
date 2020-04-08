2e0e7c6f34bdc08e8e462f5eb76d0692fce6b26f
Michael Bianchi <mbianchi@opploans.com>
2020-04-08 23:12:12 +0000

    Fix devcontainer and attempt parquet crate

--- wrangler/native/parquetserializer/src/lib.rs
+++ wrangler/native/parquetserializer/src/lib.rs
@@ -1,7 +1,12 @@
+#[macro_use]
+extern crate rustler;
+// extern crate parquet;
+// Including this causes a massive error that starts with: error: linking with `cc` failed: exit code: 1
+
 use rustler::{Encoder, Env, Error, Term};
 
 mod atoms {
-    rustler::rustler_atoms! {
+    rustler_atoms! {
         atom ok;
         //atom error;
         //atom __true__ = "true";
@@ -12,7 +17,8 @@ mod atoms {
 rustler::rustler_export_nifs! {
     "Elixir.Wrangler.ParquetSerializer",
     [
-        ("add", 2, add)
+        ("add", 2, add),
+        ("write_parquet", 2, write_parquet)
     ],
     None
 }
@@ -22,4 +28,14 @@ fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     let num2: i64 = args[1].decode()?;
 
     Ok((atoms::ok(), num1 + num2).encode(env))
+}
+
+fn write_parquet<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+    // let path: String = args[0].decode()?;
+    let path_str: String = args[0].decode()?;
+    let _num2: i64 = args[1].decode()?;
+    println!("{}", path_str);
+
+
+    Ok((atoms::ok(), 0).encode(env))
 }
